f0460d55d5db1251bb7e2e287e1e8552a44b2e7d
Mark Artishuk <markyshuk@gmail.com>
2020-08-03 09:33:51 -0700

    implemented /test route

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/test", (req, res) => {
-  if (req.hasOwnProperty("string_to_cut")) {
+  if (req.body.hasOwnProperty("string_to_cut")) {
+    const { string_to_cut } = req.body;
+
+    const return_object = {
+      return_string: getEveryThirdLetter(string_to_cut),
+    };
+
+    res.status(200).json(return_object);
   }
 });
+
+function getEveryThirdLetter(string_to_cut) {
+  let new_string = "";
+  for (let i = 0; i < string_to_cut.length; i++) {
+    if ((i + 1) % 3 !== 0) continue;
+
+    new_string += string_to_cut[i];
+  }
+
+  return new_string;
+}
 
 app.listen(port, () => {
   console.log(`Server started. Listening on port ${port}`);
